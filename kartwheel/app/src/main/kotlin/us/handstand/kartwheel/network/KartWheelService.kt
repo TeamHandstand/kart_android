913e62a2f59f8e5e61ab4613e1918977c355ced7
Matthew Ott <matthew.w.ott@gmail.com>
2017-06-22 13:12:37 -0700

    add staging api endpoint and variant

--- kartwheel/app/src/main/kotlin/us/handstand/kartwheel/network/KartWheelService.kt
+++ kartwheel/app/src/main/kotlin/us/handstand/kartwheel/network/KartWheelService.kt
@@ -5,36 +5,37 @@ import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.http.*
+import us.handstand.kartwheel.BuildConfig
 
 internal interface KartWheelService {
 
-    @POST("tickets/claim")
+    @POST(BuildConfig.SERVER_FRAGMENT + "tickets/claim")
     fun claimTicket(@Body ticketCode: JsonObject): Call<JsonElement>
 
-    @PUT("events/{eventId}/users/{userId}")
+    @PUT(BuildConfig.SERVER_FRAGMENT + "events/{eventId}/users/{userId}")
     fun updateUser(@Path("userId") userId: String, @Path("eventId") eventId: String, @Body user: JsonObject): Call<JsonElement>
 
-    @POST("events/{eventId}/tickets/{ticketId}/forfeit")
+    @POST(BuildConfig.SERVER_FRAGMENT + "events/{eventId}/tickets/{ticketId}/forfeit")
     fun forfeitTicket(@Path("eventId") eventId: String, @Path("ticketId") ticketId: String): Call<JsonElement>
 
-    @GET("events/{eventId}/races")
+    @GET(BuildConfig.SERVER_FRAGMENT + "events/{eventId}/races")
     fun getRaces(@Path("eventId") eventId: String): Call<JsonElement>
 
-    @GET("events/{eventId}/courses")
+    @GET(BuildConfig.SERVER_FRAGMENT + "events/{eventId}/courses")
     fun getCourses(@Path("eventId") eventId: String): Call<JsonElement>
 
-    @GET("events/{eventId}")
+    @GET(BuildConfig.SERVER_FRAGMENT + "events/{eventId}")
     fun getEvent(@Path("eventId") eventId: String): Call<JsonElement>
 
-    @GET("events/{eventId}/races/{raceId}/users")
+    @GET(BuildConfig.SERVER_FRAGMENT + "events/{eventId}/races/{raceId}/users")
     fun getRaceParticipants(@Path("eventId") eventId: String, @Path("raceId") raceId: String): Call<JsonElement>
 
-    @POST("events/{eventId}/races/{raceId}/join")
+    @POST(BuildConfig.SERVER_FRAGMENT + "events/{eventId}/races/{raceId}/join")
     fun joinRace(@Path("eventId") eventId: String, @Path("raceId") raceId: String): Call<JsonElement>
 
-    @POST("events/{eventId}/races/{raceId}/leave")
+    @POST(BuildConfig.SERVER_FRAGMENT + "events/{eventId}/races/{raceId}/leave")
     fun leaveRace(@Path("eventId") eventId: String, @Path("raceId") raceId: String): Call<JsonElement>
 
-    @GET("mini_game_types")
+    @GET(BuildConfig.SERVER_FRAGMENT + "mini_game_types")
     fun getMiniGameTypes(): Call<JsonElement>
 }
