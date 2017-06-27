1480701f8cc085b113f6cc39d4da645771ea3c21
Matthew Ott <matthew.w.ott@gmail.com>
2017-06-27 03:50:39 -0700

    Fully qualified imports in CodeEntryFragment

--- kartwheel/app/src/main/kotlin/us/handstand/kartwheel/fragment/ticket/CodeEntryFragment.kt
+++ kartwheel/app/src/main/kotlin/us/handstand/kartwheel/fragment/ticket/CodeEntryFragment.kt
@@ -1,54 +1,67 @@
 package us.handstand.kartwheel.fragment.ticket
 
 
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.widget.ContentLoadingProgressBar
+import android.text.Editable
 import android.text.TextUtils.isEmpty
-import android.view.KeyEvent.ACTION_DOWN
+import android.text.TextWatcher
+import android.view.KeyEvent
+import android.view.KeyEvent.ACTION_UP
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo.IME_ACTION_GO
+import android.widget.EditText
+import android.widget.TextView
 import us.handstand.kartwheel.R
+import us.handstand.kartwheel.activity.TicketActivity
+import us.handstand.kartwheel.layout.ViewUtil.findView
 
-class CodeEntryFragment : android.support.v4.app.Fragment(), us.handstand.kartwheel.activity.TicketActivity.TicketFragment, android.widget.TextView.OnEditorActionListener, android.text.TextWatcher {
-    internal var codeEntry: android.widget.EditText? = null
+class CodeEntryFragment : Fragment(), TicketActivity.TicketFragment, TextView.OnEditorActionListener, TextWatcher {
+    private lateinit var codeEntry: EditText
+    private lateinit var progress: ContentLoadingProgressBar
 
-    var progress: android.support.v4.widget.ContentLoadingProgressBar? = null
-    override fun onCreateView(inflater: android.view.LayoutInflater?, container: android.view.ViewGroup?, savedInstanceState: android.os.Bundle?): android.view.View? {
-        val fragmentView = inflater!!.inflate(us.handstand.kartwheel.R.layout.fragment_code_entry, container, false) as android.view.ViewGroup
-        codeEntry = us.handstand.kartwheel.layout.ViewUtil.findView(fragmentView, R.id.code_edit_text)
-        progress = us.handstand.kartwheel.layout.ViewUtil.findView(fragmentView, R.id.networkProgress)
-        codeEntry?.setOnEditorActionListener(this)
-        codeEntry?.addTextChangedListener(this)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val fragmentView = inflater.inflate(R.layout.fragment_code_entry, container, false) as ViewGroup
+        codeEntry = findView(fragmentView, R.id.code_edit_text)
+        progress = findView(fragmentView, R.id.networkProgress)
+        codeEntry.setOnEditorActionListener(this)
+        codeEntry.addTextChangedListener(this)
         return fragmentView
     }
 
     override fun getTitleResId(): Int {
-        return us.handstand.kartwheel.R.string.app_name
+        return R.string.app_name
     }
 
     override fun getAdvanceButtonTextResId(): Int {
-        return us.handstand.kartwheel.R.string.lets_go
+        return R.string.lets_go
     }
 
     override fun getAdvanceButtonColor(): Int {
-        return if (isAdvanceButtonEnabled()) us.handstand.kartwheel.R.color.blue else super.getAdvanceButtonColor()
+        return if (isAdvanceButtonEnabled()) R.color.blue else super.getAdvanceButtonColor()
     }
 
     override fun isAdvanceButtonEnabled(): Boolean {
-        return !isEmpty(codeEntry?.text?.toString())
+        return !isEmpty(codeEntry.text?.toString())
     }
 
     fun setProgressVisibility(visibility: Int) {
-        progress?.visibility = visibility
+        progress.visibility = visibility
     }
 
-    override fun onEditorAction(v: android.widget.TextView?, actionId: Int, event: android.view.KeyEvent?): Boolean {
-        if (actionId == IME_ACTION_GO || event?.action == ACTION_DOWN) {
-            activity.findViewById(us.handstand.kartwheel.R.id.button).performClick()
+    override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+        if (actionId == IME_ACTION_GO || event?.action == ACTION_UP) {
+            activity.findViewById(R.id.button).performClick()
             return true
         }
         return false
     }
 
-    override fun afterTextChanged(s: android.text.Editable?) {
-        ticketController.code = codeEntry!!.text.toString().toLowerCase().replace(" ", "")
+    override fun afterTextChanged(s: Editable?) {
+        ticketController.code = codeEntry.text.toString().toLowerCase().replace(" ", "")
         return ticketController.onTicketFragmentStateChanged()
     }
 
