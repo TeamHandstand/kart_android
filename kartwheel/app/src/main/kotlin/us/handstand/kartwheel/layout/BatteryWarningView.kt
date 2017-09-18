cd1ef90a4b062d3a2047a917c012e8b3a26fcccc
Matthew Ott <matthew.w.ott@gmail.com>
2017-09-17 17:02:09 -0700

    ButterKnife on BatteryWarningLevel

--- kartwheel/app/src/main/kotlin/us/handstand/kartwheel/layout/BatteryWarningView.kt
+++ kartwheel/app/src/main/kotlin/us/handstand/kartwheel/layout/BatteryWarningView.kt
@@ -5,6 +5,8 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
 import us.handstand.kartwheel.R
 
 
@@ -13,25 +15,22 @@ class BatteryWarningView : LinearLayout {
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-    var batteryPercentage: TextView
-    var batteryDescription: TextView
+    @BindView(R.id.batteryPercentage) lateinit var batteryPercentage: TextView
+    @BindView(R.id.batteryDescription) lateinit var batteryDescription: TextView
 
     init {
         View.inflate(context, R.layout.view_battery_warning, this)
-        batteryPercentage = ViewUtil.findView(this, R.id.batteryPercentage)
-        batteryDescription = ViewUtil.findView(this, R.id.batteryDescription)
+        ButterKnife.bind(this)
         if (isInEditMode) {
             setBatteryPercentage(50)
         }
     }
 
     fun setBatteryPercentage(level: Int) {
-        if (level < 20) {
-            batteryDescription.text = String(Character.toChars(0x1F6A8)) + "Charge up! You need 20% to run a race!"
-        } else if (level < 50) {
-            batteryDescription.text = String(Character.toChars(0x26A0)) + "You should have 20% to run a race!"
-        } else {
-            batteryDescription.text = "Meet at the " + String(Character.toChars(0x1F3C1)) + " at race time!"
+        when {
+            level < 20 -> batteryDescription.text = String(Character.toChars(0x1F6A8)) + "Charge up! You need 20% to run a race!"
+            level < 50 -> batteryDescription.text = String(Character.toChars(0x26A0)) + "You should have 20% to run a race!"
+            else -> batteryDescription.text = "Meet at the " + String(Character.toChars(0x1F3C1)) + " at race time!"
         }
         batteryPercentage.text = level.toString() + "%"
     }
