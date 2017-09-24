a15cf2898fde511d6938e97a66c30b71ebef80e2
Matthew Ott <matthew.w.ott@gmail.com>
2017-09-24 10:35:45 -0700

    Check device location settings before requesting location updates

--- kartwheel/app/src/main/kotlin/us/handstand/kartwheel/location/UserLocation.kt
+++ kartwheel/app/src/main/kotlin/us/handstand/kartwheel/location/UserLocation.kt
@@ -2,13 +2,16 @@ package us.handstand.kartwheel.location
 
 
 import android.annotation.SuppressLint
-import android.content.Context
+import android.app.Activity
+import android.content.IntentSender
 import android.location.Location
 import android.os.Looper
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.tasks.Task
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.subjects.PublishSubject
@@ -18,10 +21,16 @@ import us.handstand.kartwheel.model.UserRaceInfo
 import us.handstand.kartwheel.model.UserRaceInfoModel
 import us.handstand.kartwheel.network.API
 import us.handstand.kartwheel.util.Permissions
+import us.handstand.kartwheel.util.SnackbarUtil
 
-class UserLocation(val context: Context) : Observable<Location>() {
-    private var publisher = PublishSubject.create<Location>()
-    private val locationProvider = FusedLocationProviderClient(context)
+class UserLocation(val activity: Activity) : Observable<Location>() {
+    private val CHECK_SETTINGS_REQUEST = 102
+    private val publisher = PublishSubject.create<Location>()
+    private val locationProvider = FusedLocationProviderClient(activity)
+    private val locationRequest = LocationRequest()
+            .setInterval(1000)
+            .setFastestInterval(400)
+            .setPriority(PRIORITY_HIGH_ACCURACY)
 
     private val locationCallbacks = object : LocationCallback() {
         override fun onLocationResult(result: LocationResult) {
@@ -50,8 +59,30 @@ class UserLocation(val context: Context) : Observable<Location>() {
 
     @SuppressLint("MissingPermission")
     fun requestLocationUpdates() {
-        if (Permissions.hasLocationPermissions(context)) {
-            locationProvider.requestLocationUpdates(LocationRequest().setInterval(1000), locationCallbacks, Looper.getMainLooper())
+        if (Permissions.hasLocationPermissions(activity)) {
+            val settingsResponse = checkDeviceLocationSettings()
+            settingsResponse.addOnCompleteListener {
+                locationProvider.requestLocationUpdates(locationRequest, locationCallbacks, Looper.getMainLooper())
+            }
         }
+    }
+
+    private fun checkDeviceLocationSettings(): Task<LocationSettingsResponse> {
+        val settingsRequest = LocationSettingsRequest.Builder().addLocationRequest(locationRequest).build()
+        val settingsResponse = LocationServices.getSettingsClient(activity).checkLocationSettings(settingsRequest)
+        settingsResponse.addOnFailureListener {
+            when ((it as ApiException).statusCode) {
+                CommonStatusCodes.RESOLUTION_REQUIRED -> {
+                    try {
+                        (it as ResolvableApiException).startResolutionForResult(activity, CHECK_SETTINGS_REQUEST)
+                    } catch (e: IntentSender.SendIntentException) {
+                    }
+                }
+                LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                    SnackbarUtil.show(activity, "Location settings not satisfied, but unable to fix the settings.")
+                }
+            }
+        }
+        return settingsResponse
     }
 }
