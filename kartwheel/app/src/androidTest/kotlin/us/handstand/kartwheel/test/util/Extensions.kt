1b7183cf36c054ebde7333030f4ccd442188b49b
Matthew Ott <matthew.w.ott@gmail.com>
2018-05-28 12:40:48 -0700

    Added RaceListFragmentTest

---
+++ kartwheel/app/src/androidTest/kotlin/us/handstand/kartwheel/test/util/Extensions.kt
@@ -0,0 +1,21 @@
+package us.handstand.kartwheel.test.util
+
+import android.graphics.drawable.ShapeDrawable
+import android.support.test.espresso.intent.Checks
+import android.view.View
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+
+
+fun withBackground(background: Int): Matcher<View> {
+    Checks.checkNotNull(background)
+    return object: TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description) {
+            description.appendText("with background: ")
+        }
+
+        override fun matchesSafely(item: View): Boolean =
+                (item.background as ShapeDrawable).paint.color == background
+    }
+}
